fd2b9a3137e2bf7125495e82bdae89f730b5dd77
Satchapum Krailertrat <satchapum.mail@mail.kmutt.ac.th>
2023-02-02 19:54:13 +0700

    Finish

---
+++ Q1/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program {
+    public static void Main(string[] args) {
+        double inputX = double.Parse(Console.ReadLine());
+        Console.WriteLine(Calculate(inputX));
+    }
+    public static double Calculate(double inputX) {
+        if(Math.Pow(inputX,2) > 4){
+            inputX = 1+ Calculate(inputX - (4*Math.Abs(inputX))/(inputX));
+            return inputX;
+        }
+        else {
+            inputX = 2;
+            return inputX;
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program {
+    public static void Main(string[] args) {
+        double CX = double.Parse(Console.ReadLine());
+        double CY = double.Parse(Console.ReadLine());
+        double PX = double.Parse(Console.ReadLine());
+        double PY = double.Parse(Console.ReadLine());
+        double Round = double.Parse(Console.ReadLine());
+        double FirstRadius = Math.Sqrt(Math.Pow(CX-PX,2)+Math.Pow(CY-PY,2));
+        double FirstSeta = (180/Math.PI)*(Math.Atan((CX-PX)/(CY-PY)));
+
+        while(Round > 0) {
+            FirstSeta = FirstSeta + 90;
+            PX = ((FirstRadius)*Math.Cos(FirstSeta/(180/Math.PI)))+CX;
+            PY = ((FirstRadius)*Math.Sin(FirstSeta/(180/Math.PI)))+CY;
+            FirstRadius = FirstRadius/2;
+            CX = ((FirstRadius)*Math.Cos(FirstSeta/(180/Math.PI)))+CX;
+            CY = ((FirstRadius)*Math.Sin(FirstSeta/(180/Math.PI)))+CY;
+            Round--;
+        }
+        Console.WriteLine(CX);
+        Console.WriteLine(CY);
+        Console.WriteLine(PX);
+        Console.WriteLine(PY);
+    }
+}
