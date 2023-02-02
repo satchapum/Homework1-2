da6437e96f5d8e9bcfdcacfcf03819b689b3415b
Satchapum Krailertrat <satchapum.mail@mail.kmutt.ac.th>
2023-02-02 20:54:02 +0700

    fix Q3

--- Q3/Program.cs
+++ Q3/Program.cs
@@ -1,7 +1,6 @@
 ﻿class Program {
     public static void Main(string[] args) {
         double InputX = double.Parse(Console.ReadLine());
-        InputX = InputX % 2;
         Console.WriteLine(Calculate(InputX));
     }
     public static double Calculate(double x) {
@@ -12,12 +11,12 @@
             return Calculate(x-2);
         }
         else if(x > 0 && x <= 1) {
-            return Math.Sqrt(1-Math.Pow(x,2));
-        }
-        else if(x <= 0 && x >-1) {
             return -Math.Sqrt(1-Math.Pow(x,2));
         }
-        else {
+        else if(x <= 0 && x >-1) {
+            return Math.Sqrt(1-Math.Pow(x,2));
+        }
+        else { 
             return x;
         }
     }
